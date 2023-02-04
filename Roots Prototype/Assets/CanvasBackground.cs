1bf9692c796b16a28b31c9ee0133eed76923b630
weepingwolfs <124415925+weepingwolfs@users.noreply.github.com>
2023-02-04 13:55:29 -0500

    kkj

---
+++ Roots Prototype/Assets/CanvasBackground.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CanvasBackground : MonoBehaviour
+{
+    /*
+    void Start()
+    {
+        transform.position = transform.parent.position;
+        transform.localScale = transform.parent.localScale;
+       RectTransform tfm = this.transform.gameObject.GetComponent<RectTransform>(); 
+       RectTransform ptfm = canvas.GetComponent<RectTransform>();
+       tfm.sizeDelta.y = ptfm.rect.height;
+       tfm.sizeDelta.x = ptfm.rect.width;
+    }*/
+}
